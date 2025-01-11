1f41bcb85bd9423cb597a23b238445a98a91fb9b
ericnabofa <111744463+ericnabofa@users.noreply.github.com>
2025-01-11 16:30:45 +0000

    cleans repo

--- cypress/support/my_test_spec.js
+++
@@ -1,8 +0,0 @@
-// cypress/integration/my_test_spec.js
-describe('My First Test', () => {
-    it('Should visit the homepage and check the title', () => {
-      cy.visit('https://example.com');
-      cy.title().should('include', 'Example Domain');
-    });
-  });
-  
--- outlierDualCachineSecondResponse/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/src/Cache.php';
-require_once __DIR__ . '/src/ErrorHandler.php';
-require_once __DIR__ . '/src/CacheMonitor.php';
-
-$cache = new Cache(__DIR__ . '/cache/');
-$cache->set('test_key', 'test_value');
-echo $cache->get('test_key');
